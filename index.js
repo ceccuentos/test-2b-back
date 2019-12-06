63ccf408088bda1fa361e103323d7173e461534a
Cec <cec@Asure-Kunga-3.local>
2019-12-05 21:20:47 -0300

    add redis

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ import cors from 'cors';
 import path from 'path';
 //import axios from 'axios';
 import request from 'request-promise';
-
+import redis from 'redis';
 import logger from './utils/logger';
 import winston from 'winston';
 
 import { config } from 'dotenv';
 
-
 const routeApp = express();
 config();
 
 //Middelware
-//routeApp.use(morgan('tiny'));
-logger.debug("Overriding 'Express' logger");
 routeApp.use(morgan('combined', { stream: winston.stream.write }));
 
 routeApp.use(cors());
@@ -24,21 +21,37 @@ routeApp.use(express.json());
 //application/x-www-form-urlencoded
 routeApp.use(express.urlencoded({ extended: true }));
 
-//Middleware
 import {verificarAuth} from './middlewares/auth';
 
-
+export const client = redis.createClient(process.env.URL_REDIS)
+console.log(process.env.URL_REDIS)
+const keyRedis = 'key:name'
 // Rutas
 /* routeApp.get('/', (req, res) => {
     res.send('Test 2B!');
   }); */
   
   routeApp.get('/api/getnames', [verificarAuth], async(req, res, next) => {
-    if (Math.random() < 0.15) {
+    if (Math.random() < 0.55) {
       let errorMessage = `Error Code: 500\nMessage: Servicio no disponible`;
       logger.error(`500 - ${errorMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
       res.status(503).send('Service No disponible');
     } else {
+      // Get From Redis
+      client.get(keyRedis, function(err, data) {
+        if (data) {
+          const resultJSON = JSON.parse(data);
+          return res.status(200).json(resultJSON);
+        } else {
+          request(process.env.URL_API, { json: true }, (err, res2, body) => {  
+            if (err) { res.status(503).send(err); }
+            const responseJSON = body;
+            client.setex(keyRedis, 120, JSON.stringify(responseJSON));
+            res.status(200).send(body);
+          });
+        }
+      })
+
 /*         await axios.get(process.env.URL_API,{
           responseType: 'json'
         })
@@ -50,10 +63,7 @@ import {verificarAuth} from './middlewares/auth';
         })
         //return res.data || [];
     } */
-      request(process.env.URL_API, { json: true }, (err, res2, body) => {  
-        if (err) { res.status(503).send(err); }
-        res.status(200).send(body);
-      });
+
     }  
 
 
